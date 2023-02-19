37c393d2296e1e177ce776d8a6b718ab08dc4492
gmfaruk <gmfaruk2021@gmail.com>
2023-02-19 22:39:46 +0600

    zigzag converion

---
+++ Zigzag Conversion/zigzag_conversion.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        # Base condition
+        if s is None and numRows <= 0:
+            return ""
+        if numRows == 1:
+            return s
+        # Resultant string
+        result = ""
+        # Step size
+        step = 2 * numRows - 2
+        # Loop for each row
+        for i in range(0, numRows):
+            # Loop for each character in the row
+            for j in range(i, len(s), step):
+                result += s[j]
+                if i != 0 and i != numRows - 1 and (j + step - 2 * i) < len(s):
+                    result += s[j + step - 2 * i]
+        return result
